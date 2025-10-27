ac1f328656417ae3fe8ba3ffb3c406ce58ede7ef
seowoosung <ddungcum@gmail.com>
2025-10-27 21:54:58 +0900

    question4 수정

--- src/pages/tests/question4.tsx
+++ src/pages/tests/question4.tsx
@@ -2,55 +2,47 @@ import { getDefaultLayout } from "@/components/layout/default-layout";
 import { updateCount } from "@/redux/slices";
 import { TRootState } from "@/redux/store";
 import { Button, Divider } from "antd";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 const Question4 = () => {
   const dispatch = useDispatch();
   const [count, setCount] = useState(0);
-  const countRef = useRef<number>();
   let countLet = 0;
 
   const handleClick = () => {
     setCount(count + 1);
-    countRef.current = count + 1;
     countLet += 1;
   };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       dispatch(updateCount(countLet));
-    }, 3000);
+    }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div>
+    <>
       <div className="text-base mb-10 text-customGray-800">
-        Q. Increment를 한번 눌렀을때 4개 값은? 두번 눌렀을때는?
+        Q1. Increment를 한번 누르고 1초 뒤에 3개 값은? 두번 눌렀을때는?
+        {/* 만약 setCount를 지우고 똑같이 한다면 그 결과는? */}
       </div>
-      <div className="text-lg font-semibold mb-4">Count: {count}</div>
-      <div className="text-lg font-semibold mb-4">CountRef: {countRef.current}</div>
-      <div className="text-lg font-semibold mb-4">CountLet: {countLet}</div>
+      <div className="text-lg mb-4">Count: {count}</div>
+      <div className="text-lg mb-4">CountLet: {countLet}</div>
       <Child />
-      <div className="flex flex-col gap-3 w-fit">
-        <Button onClick={handleClick} type="primary">
-          Increment
-        </Button>
-      </div>
+      <Button onClick={handleClick} type="primary">
+        Increment
+      </Button>
       <Divider />
-    </div>
+    </>
   );
 };
 
 const Child = () => {
   const reduxCount = useSelector((state: TRootState) => state.counter.count, shallowEqual);
-  return (
-    <>
-      <div className="text-lg font-semibold mb-4">reduxCount: {reduxCount}</div>
-    </>
-  );
+  return <div className="text-lg mb-4">reduxCount: {reduxCount}</div>;
 };
 
 Question4.getLayout = getDefaultLayout;
